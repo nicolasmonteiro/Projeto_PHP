88f28510107a11f451a059b7ab72039326bf25af
Nicolas Monteiro <74384092+nicolasmonteiro@users.noreply.github.com>
2021-01-13 01:34:36 -0300

    Data de atualização- 13/01

---
+++ Projeto Feira - Versão 2/basic.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Feira Delivery</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href= "css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</head>
+
+<body>
+<nav>
+    <div class="nav-wrapper green darken-3">
+      <a href="#" class="brand-logo">Feira</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Frutas</a></li>
+        <li><a href="#">Verduras/legumes</a></li>
+        <li><a href="#">Folhagens</a></li>
+        <li><a href="#">Raízes/Tubérculos</a></li>
+        <li><a href="#"><i class="material-icons left">account_circle</i>Entrar</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+
+</html>
