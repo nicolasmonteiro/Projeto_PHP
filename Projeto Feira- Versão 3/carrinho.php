82a7bcf681b7cbec502aace444b68c8c1626168b
Nicolas Monteiro <74384092+nicolasmonteiro@users.noreply.github.com>
2021-01-18 23:56:54 -0300

    Modificado - 18/01/2021

---
+++ Projeto Feira- Versão 3/carrinho.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Select Hortifruti</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href= "css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="images/FavIcon/favicon.ico">
+
+  
+</head>
+
+<body>
+<nav>
+    <div class="nav-wrapper green darken-3">
+    <a id="logo-container" href="index.php" class="brand-logo"><img src="images/logo/logo.png" width="60" height="60" alt="" title="Feira Verde">Select Hortifruti</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="frutas.php">Frutas</a></li>
+        <li><a href="verduras.php">Verduras/legumes</a></li>
+        <li><a href="folhagens.php">Folhagens</a></li>
+        <li><a href="raizes.php">Raízes/Tubérculos</a></li>
+        <li><a href="login.php"><i class="material-icons left">account_circle</i>Entrar</a></li>
+        <li><a href="#"><i class="material-icons left">add_shopping_cart</i>Carrinho</a></li>
+        <li><a href="#"><i class="material-icons left">exit_to_app</i>Sair</a></li>
+      </ul>
+    </div>
+</nav>
+ <div class="container center">
+    <div class="box center">
+      <div class="card center">
+        <div class="card-content black-text">
+          <span class="card-title">Carrinho</span>
+          <label class="label-input" for="">
+              <i class="far fa-envelope icon-modify"></i>
+              <input type="text" placeholder="Nome">
+          </label>
+          <label class="label-input" for="">
+              <i class="far fa-envelope icon-modify"></i>
+              <input type="text" placeholder="Endereço">
+          </label>
+          <label class="label-input" for="">
+              <i class="far fa-envelope icon-modify"></i>
+              <input type="text" placeholder="Data">
+          </label>
+           <label class="label-input" for="">
+              <i class="far fa-envelope icon-modify"></i>
+              <input type="tel" placeholder="Forma de pagamento">
+          </label> 
+
+          <a button class="btn btn-second btn-small green darken-3" href="#">Finalizar pedido</a> 
+  <body>
+</html>
---
+++ Projeto Feira- Versão 3/folhagens.php
@@ -0,0 +1,79 @@
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>Select Hortifruti</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href= "css/style.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="images/FavIcon/favicon.ico">
+</head>
+
+<body>
+<nav>
+    <div class="nav-wrapper green darken-3">
+    <a id="logo-container" href="index.php" class="brand-logo"><img src="images/logo/logo.png" width="60" height="60" alt="" title="Feira Verde">Select Hortifruti</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="frutas.php">Frutas</a></li>
+        <li><a href="verduras.php">Verduras/legumes</a></li>
+        <li><a href="#">Folhagens</a></li>
+        <li><a href="raizes.php">Raízes/Tubérculos</a></li>
+        <li><a href="login.php"><i class="material-icons left">account_circle</i>Entrar</a></li>
+        <li><a href="carrinho.php"><i class="material-icons left">add_shopping_cart</i>Carrinho</a></li>
+        <li><a href="#"><i class="material-icons left">exit_to_app</i>Sair</a></li>
+      </ul>
+    </div>
+  </nav>
+<table class = "centered">
+      <thead >
+        <tr>
+            <th>Visual</th>
+            <th>Produto</th>
+            <th>Setor</th>
+            <th>Preço</th>
+            <th>  </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td><img src="images/icons/folhagens/cebolinha.webp" width="49.84" height="49" alt=""></td>
+          <td>Cebolinha</td>
+          <td>Folhagens</td>
+          <td>R$ 3.65 Un.</td>
+          <td><a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">add</i></a>              <a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">remove</i></a></td>
+        </tr>
+        <tr>
+          <td><img src="images/icons/folhagens/alface.png" width="49.84" height="49" alt=""></td>
+          <td>Alface Crespa</td>
+          <td>Folhagens</td>
+          <td>R$ 3.68 Un.</td>
+          <td><a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">add</i></a>              <a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">remove</i></a></td>
+        </tr>
+        <tr>
+          <td><img src="images/icons/folhagens/coentro.png" width="49.84" height="49" alt=""></td>
+          <td>Coentro</td>
+          <td>Folhagens</td>
+          <td>R$ 2.59 Un.</td>
+          <td><a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">add</i></a>              <a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">remove</i></a></td>
+        </tr>
+        <tr>
+          <td><img src="images/icons/folhagens/couve.png" width="49.84" height="49" alt=""></td>
+          <td>Couve Manteiga</td>
+          <td>Folhagens</td>
+          <td>R$ 1.99 Un.</td>
+          <td><a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">add</i></a>              <a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">remove</i></a></td>
+        </tr>
+        <tr>
+          <td><img src="images/icons/folhagens/repolho.png" width="49.84" height="49" alt=""></td>
+          <td>Repolho Verde</td>
+          <td>Folhagens</td>
+          <td>R$ 2.54/kg</td>
+          <td><a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">add</i></a>              <a class="btn-floating btn-small waves-effect waves- green darken-3"><i class="material-icons">remove</i></a></td>
+        </tr>
+      </tbody>
+    </table>
+</body>
+
+</html>
